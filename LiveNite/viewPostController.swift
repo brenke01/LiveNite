8df7edfaf4f34dc7ce818de8606175d33bb5c165
Jacob Pierce <pierc358@gmail.com>
2016-01-14 21:33:51 -0600

    Upvote and downvote functionality added with logic to ensure users can't vote multiple times but can change their votes as desired.

--- LiveNite/viewPostController.swift
+++ LiveNite/viewPostController.swift
@@ -55,7 +55,42 @@ class viewPostController: UIViewController, UIImagePickerControllerDelegate, UIN
         let navBarBGImage = UIImage(named: "Navigation_Bar_Gold")
         navigationBar.setBackgroundImage(navBarBGImage, forBarMetrics: .Default)
         navigationBar.topItem!.title = imageTitle
-        
+        var userUpvoteStatus : Int = 0
+        var currentUserName : String = ""
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+                
+            }
+            else
+            {
+                currentUserName = result.valueForKey("id") as! String
+            }
+        })
+        let userUpvoteStatusFetchRequest = NSFetchRequest(entityName: "UserUpvotes")
+        let userUpvoteStatusFetchResults = (try? context.executeFetchRequest(userUpvoteStatusFetchRequest)) as! [NSManagedObject]?
+        if let userUpvoteStatusFetchResults = userUpvoteStatusFetchResults{
+            for result in userUpvoteStatusFetchResults{
+                let idData : AnyObject? = result.valueForKey("image_id")
+                let id = idData as! Int
+                let upvoteData : AnyObject? = result.valueForKey("upvote_value")
+                let upvoteStatus = upvoteData as! Int
+                let userData : AnyObject? = result.valueForKey("user_name")
+                let userName = userData as! String
+                if userName == currentUserName && id == imageID {
+                    userUpvoteStatus = upvoteStatus
+                }
+            }
+        }
+        if userUpvoteStatus == 1{
+            upvoteButton.alpha = 0.5
+        } else if userUpvoteStatus == -1{
+            downvoteButton.alpha = 0.5
+        }
     }
     
     func loadImageDetail(){
@@ -77,18 +112,46 @@ class viewPostController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     
     func UpVote(sender: UIButton){
-        print("Upvote")
-        print(sender.tag, terminator: "")
-        let id = 0
-        let disableMyButton = sender as UIButton
-        disableMyButton.enabled = false
-        disableMyButton.alpha = 0.5
+        //get current user name
+        var userUpvoteStatus : Int = 0
+        var currentUserName : String = ""
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+                
+            }
+            else
+            {
+                currentUserName = result.valueForKey("id") as! String
+            }
+        })
+        //get user upvote status
+        let userUpvoteStatusFetchRequest = NSFetchRequest(entityName: "UserUpvotes")
+        let userUpvoteStatusFetchResults = (try? context.executeFetchRequest(userUpvoteStatusFetchRequest)) as! [NSManagedObject]?
+        if let userUpvoteStatusFetchResults = userUpvoteStatusFetchResults{
+            for result in userUpvoteStatusFetchResults{
+                let idData : AnyObject? = result.valueForKey("image_id")
+                let id = idData as! Int
+                let upvoteData : AnyObject? = result.valueForKey("upvote_value")
+                let upvoteStatus = upvoteData as! Int
+                let userData : AnyObject? = result.valueForKey("user_name")
+                let userName = userData as! String
+                if userName == currentUserName && id == imageID {
+                    userUpvoteStatus = upvoteStatus
+                }
+            }
+        }
+        //get image upvote data and manipulate based on upvote status
         let fetchRequest = NSFetchRequest(entityName: "Entity")
         fetchRequest.predicate = NSPredicate(format: "id = %i", sender.tag)
+        print(fetchRequest.predicate)
         let locations = (try? context.executeFetchRequest(fetchRequest)) as! [NSManagedObject]?
         var upvote = 0
         if let locations = locations{
-            
             for loc in locations{
                 print("Loop")
                 let idData : AnyObject? = loc.valueForKey("id")
@@ -96,69 +159,139 @@ class viewPostController: UIViewController, UIImagePickerControllerDelegate, UIN
                 print(id, terminator: "")
                 let upvoteData : AnyObject? = loc.valueForKey("upvotes")
                 upvote = upvoteData as! Int
-                upvote = upvote + 1
+                if userUpvoteStatus == 0 {
+                    upvote = upvote + 1
+                    userUpvoteStatus = 1
+                    upvoteButton.alpha = 0.5
+                } else if userUpvoteStatus == 1 {
+                    upvote = upvote - 1
+                    userUpvoteStatus = 0
+                    upvoteButton.alpha = 1.0
+                } else if userUpvoteStatus == -1 {
+                    upvote = upvote + 2
+                    userUpvoteStatus = 1
+                    upvoteButton.alpha = 0.5
+                    downvoteButton.alpha = 1.0
+                } else {
+                    print("userUpvoteStatus is not a valid number")
+                }
                 
                 loc.setValue(upvote, forKey: "upvotes")
                 do {
                     try context.save()
                 } catch _ {
                 }
-                
-                
-                
             }
-            
-            
-            
-            
-            
-            
         }
-        userUpvoted(id)
+        print(imageID)
+        //save data in core data
+        userVoted(imageID, user_name: currentUserName, upvote_value: userUpvoteStatus)
         upvotesLabel.text = String(upvote)
         //self.collectionView!.reloadData()
         
     }
     
-    func userUpvoted(id : Int){
-        
+    func userVoted(id : Int, user_name : String, upvote_value : Int){
+        let fetchRequest = NSFetchRequest(entityName: "UserUpvotes")
+        fetchRequest.predicate = NSPredicate(format: "image_id= %i", id)
+        let imageVote = (try? context.executeFetchRequest(fetchRequest)) as! [NSManagedObject]?
+        //If the fetch request returns nothing, we know that it is a new image vote
+        print("user upvoted")
+        if (imageVote! == []){
+            if let newImageVote = NSEntityDescription.insertNewObjectForEntityForName("UserUpvotes", inManagedObjectContext:context) as? NSManagedObject{
+            //For each "column" set your values
+                newImageVote.setValue(imageID, forKey: "image_id")
+                newImageVote.setValue(user_name, forKey: "user_name")
+                newImageVote.setValue(upvote_value, forKey: "upvote_value")
+            }
+        } else{
+            //just change value for appropriate image user pair
+            if let imageVote = imageVote{
+                for image in imageVote{
+                    if image.valueForKey("image_id") as! Int == imageID && image.valueForKey("user_name") as! String == user_name {
+                        image.setValue(upvote_value, forKey: "upvote_value")
+                    }
+                }
+                
+            }
+        }
     }
     
     func DownVote(sender: UIButton){
-        print(sender.tag, terminator: "")
+        //get current user name
+        var userUpvoteStatus : Int = 0
+        var currentUserName : String = ""
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+                
+            }
+            else
+            {
+                currentUserName = result.valueForKey("id") as! String
+            }
+        })
+        //get upvote status
+        let userUpvoteStatusFetchRequest = NSFetchRequest(entityName: "UserUpvotes")
+        let userUpvoteStatusFetchResults = (try? context.executeFetchRequest(userUpvoteStatusFetchRequest)) as! [NSManagedObject]?
+        if let userUpvoteStatusFetchResults = userUpvoteStatusFetchResults{
+            for result in userUpvoteStatusFetchResults{
+                print("Loop in upvote")
+                let idData : AnyObject? = result.valueForKey("image_id")
+                let id = idData as! Int
+                let upvoteData : AnyObject? = result.valueForKey("upvote_value")
+                let upvoteStatus = upvoteData as! Int
+                let userData : AnyObject? = result.valueForKey("user_name")
+                let userName = userData as! String
+                if userName == currentUserName && id == imageID {
+                    userUpvoteStatus = upvoteStatus
+                }
+            }
+        }
+        //get image upvote data and manipulate based on upvote status
         let fetchRequest = NSFetchRequest(entityName: "Entity")
         fetchRequest.predicate = NSPredicate(format: "id = %i", sender.tag)
+        print(fetchRequest.predicate)
         let locations = (try? context.executeFetchRequest(fetchRequest)) as! [NSManagedObject]?
         var upvote = 0
         if let locations = locations{
-            
             for loc in locations{
-                
+                print("Loop")
                 let idData : AnyObject? = loc.valueForKey("id")
                 let id = idData as! Int
                 print(id, terminator: "")
                 let upvoteData : AnyObject? = loc.valueForKey("upvotes")
                 upvote = upvoteData as! Int
-                upvote = upvote - 1
+                if userUpvoteStatus == 0 {
+                    upvote = upvote - 1
+                    userUpvoteStatus = -1
+                    downvoteButton.alpha = 0.5
+                } else if userUpvoteStatus == 1 {
+                    upvote = upvote - 2
+                    userUpvoteStatus = -1
+                    downvoteButton.alpha = 0.5
+                    upvoteButton.alpha = 1.0
+                } else if userUpvoteStatus == -1 {
+                    upvote = upvote + 1
+                    userUpvoteStatus = 0
+                    downvoteButton.alpha = 1.0
+                } else {
+                    print("userUpvoteStatus is not a valid number")
+                }
+                
                 loc.setValue(upvote, forKey: "upvotes")
                 do {
                     try context.save()
                 } catch _ {
                 }
-                
-                
-                
             }
-            
-            
-            
-            
-            
-            
         }
+        //save data in core data
+        userVoted(imageID, user_name: currentUserName, upvote_value: userUpvoteStatus)
         upvotesLabel.text = String(upvote)
-        //self.collectionView?.reloadData()
-        
     }
-
 }
