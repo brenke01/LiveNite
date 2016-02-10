eff7a72b70936536b29ef44dc035031b33e7a93c
Jacob Pierce <pierc358@gmail.com>
2016-02-09 21:20:34 -0600

    "Slight change in user name functionality"

--- LiveNite/ViewController.swift
+++ LiveNite/ViewController.swift
@@ -425,7 +425,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.collectionView?.reloadData()
-        userID = currentUserName
         checkForUserName()
         
     }
