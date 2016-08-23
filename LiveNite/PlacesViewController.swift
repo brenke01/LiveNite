3281230da187f739af2b2f0bb365a2ea0c2f5fbd
Kevin <kevin@Kevins-Mac.local>
2016-08-22 17:06:58 -0700

    Added dynamodb integration with PlacesViewController

--- LiveNite/PlacesViewController.swift
+++ LiveNite/PlacesViewController.swift
@@ -11,10 +11,34 @@ import CoreData
 
 class PlacesViewController{
     
-    func getGroupedImages()->NSFetchRequest{
-        let fetchRequest = NSFetchRequest(entityName: "Entity")
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false), NSSortDescriptor(key: "upvotes", ascending: false)]
-        return fetchRequest
+    func getGroupedImages()->[Image]{
+        var imagesArray = [Image]
+        var condition = DynamoDBCondition()
+        var queryStartKey: [NSObject : AnyObject]? = nil
+        var placeNames = [String]
+        repeat {
+            var queryRequest: DynamoDBQueryRequest = DynamoDBQueryRequest()
+            queryRequest.tableName = "Images"
+            
+            queryRequest.exclusiveStartKey = queryStartKey!
+            
+            var queryResponse: DynamoDBQueryResponse = Constants.ddb().query(queryRequest)
+            // Each item in the result set is a NSDictionary of DynamoDBAttributeValue
+            for item: [NSObject : AnyObject] in queryResponse.items {
+                
+                var image : Image = item as Image
+                imagesArray.append(image)
+            }
+        }
+        let sortedArray = (imagesArray as NSArray).sortedArrayUsingDescriptors([
+            NSSortDescriptor(key: "placeTitle", ascending: true),
+            NSSortDescriptor(key: "totalScore", ascending: true)
+            ]) as! [Image]
+        return sortedArray
+
+//        let fetchRequest = NSFetchRequest(entityName: "Entity")
+//        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false), NSSortDescriptor(key: "upvotes", ascending: false)]
+//        return fetchRequest
     }
     
     func getImagesForGroup(placeName: String, user: User)->[Image]{
