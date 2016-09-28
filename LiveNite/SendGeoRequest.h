a1122ec542a61a00a66fa26b212e786836915d1d
Kevin <kevin@Kevins-Mac.local>
2016-09-27 18:24:16 -0700

    query optimization

--- LiveNite/SendGeoRequest.h
+++ LiveNite/SendGeoRequest.h
@@ -1,6 +1,22 @@
 
-
 #import <Foundation/Foundation.h>
+
+- (void)sendRequest:(NSDictionary *)requestDictionary {
+    NSLog(@"Request:\n%@", requestDictionary);
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:AWSElasticBeanstalkEndpoint]
+                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                       timeoutInterval:120.0];
+    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:requestDictionary
+                                                       options:kNilOptions
+                                                         error:nil];
+    request.HTTPMethod = @"POST";
+    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    if (conn) {
+        self.data = [NSMutableData data];
+        NSLog(self.data);
+    }
+}
 
 @interface SendGeoRequest : NSObject
 
