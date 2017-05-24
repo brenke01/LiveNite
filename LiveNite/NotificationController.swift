be445475727f77e96f45d8745463bea1b804aff9
Kevin <kevin@Kevins-Mac.local>
2017-05-24 04:59:36 -0700

    empty noticiation message

--- LiveNite/NotificationController.swift
+++ LiveNite/NotificationController.swift
@@ -26,6 +26,9 @@ class NotificationController: UIViewController, UITableViewDelegate, UITableView
     var chosenImage = UIImage()
     var chosenImageObj = Image()
     var activityIndicator = UIActivityIndicatorView()
+    var arrayEmpty = false
+    var emptyArrayLabel = UILabel()
+    var tryAgainButton = UILabel()
 
     
     override func viewDidLoad(){
@@ -48,35 +51,7 @@ class NotificationController: UIViewController, UITableViewDelegate, UITableView
                 AWSService().loadUser(self.userID,completion: {(result)->Void in
 
                     self.user = result
-                    self.getNotifications(completion: {(result)->Void in
-                        self.notificationArray = result
-                        self.notificationArray = (self.notificationArray as NSArray).sortedArray(using: [
-                            NSSortDescriptor(key: "actionTime", ascending: false)
-                            ]) as! [Notification]
-                        DispatchQueue.main.async(execute: {
-                        self.updateOpenNotifications(notifArray: self.notificationArray)
-                        })
-                        for n in self.notificationArray{
-                            var bucket : String
-                            if(n.type == "meetUp"){
-                                bucket = "liveniteprofileimages"
-                            }
-                            else{
-                                bucket = "liveniteimages"
-                            }
-                            AWSService().getImageFromUrl(String(n.imageID), bucket: bucket, completion: {(result)->Void in
-                                DispatchQueue.main.async(execute: {
-                                self.uiImageArr.append(result)
-
-                                    self.tableView.isHidden = false
-                                    self.tableView.reloadData()
-                                    
-                                })
-                            })
-                        }
-
-   
-                    })
+                    self.getNotificationArray()
                     
                 })
                 
@@ -87,6 +62,47 @@ class NotificationController: UIViewController, UITableViewDelegate, UITableView
   
         
         
+    }
+    
+    func getNotificationArray(){
+        self.getNotifications(completion: {(result)->Void in
+            self.notificationArray = result
+            if (self.notificationArray.count == 0){
+                self.arrayEmpty = true
+                 self.tableView.isHidden = false
+                 DispatchQueue.main.async(execute: {
+                self.tableView.reloadData()
+                })
+            }else{
+                self.arrayEmpty = false
+                self.notificationArray = (self.notificationArray as NSArray).sortedArray(using: [
+                    NSSortDescriptor(key: "actionTime", ascending: false)
+                    ]) as! [Notification]
+                DispatchQueue.main.async(execute: {
+                    self.updateOpenNotifications(notifArray: self.notificationArray)
+                })
+                for n in self.notificationArray{
+                    var bucket : String
+                    if(n.type == "meetUp"){
+                        bucket = "liveniteprofileimages"
+                    }
+                    else{
+                        bucket = "liveniteimages"
+                    }
+                    AWSService().getImageFromUrl(String(n.imageID), bucket: bucket, completion: {(result)->Void in
+                        DispatchQueue.main.async(execute: {
+                            self.uiImageArr.append(result)
+                            
+                            self.tableView.isHidden = false
+                            self.tableView.reloadData()
+                            
+                        })
+                    })
+                }
+            }
+            
+            
+        })
     }
     
     func updateOpenNotifications(notifArray : [Notification]){
@@ -112,12 +128,11 @@ class NotificationController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func handleRefresh(_ refreshControl: UIRefreshControl){
-        self.getNotifications(completion: {(result)->Void in
-            DispatchQueue.main.async(execute: {
-                refreshControl.endRefreshing()
-                self.tableView.reloadData()
-                
-            })
+        AWSService().loadUser(self.userID,completion: {(result)->Void in
+            
+            self.user = result
+            self.getNotificationArray()
+            
         })
         
     }
@@ -152,79 +167,134 @@ class NotificationController: UIViewController, UITableViewDelegate, UITableView
 
     
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return self.uiImageArr.count
-        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section : Int) -> Int{
+        if (self.arrayEmpty){
+            return 1
+        }else{
+            return self.notificationArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:NotificationCell = self.tableView.dequeueReusableCell(withIdentifier: "notificationCell")! as! NotificationCell
-        cell.notifImage.layer.borderWidth = 0
-        cell.notifImage.layer.borderColor = UIColor.clear.cgColor
-        cell.userNameLabel.text = self.notificationArray[indexPath.row].userName
-        if self.notificationArray[indexPath.row].type == "checkIn"{
-            cell.notificationLabel.text = "checked in"
-        }else if self.notificationArray[indexPath.row].type == "comment"{
-            cell.notificationLabel.text = "commented on your post."
-        }else if self.notificationArray[indexPath.row].type == "meetUp"{
-            cell.notificationLabel.text = "and you met up!"
-        }
-        let timePosted = notificationArray[(indexPath as NSIndexPath).row].actionTime
-        cell.notifImage.image = self.uiImageArr[indexPath.row]
-        cell.notifImage.contentMode = UIViewContentMode.scaleAspectFit
-        DispatchQueue.main.async(execute: {
-        if (self.notificationArray[(indexPath as NSIndexPath).row].type != "meetUp"){
-            cell.notifImage.layer.borderWidth = 2
-            cell.notifImage.layer.borderColor = UIColor.white.cgColor
-            }})
-
         cell.selectionStyle = UITableViewCellSelectionStyle.none
-        let dateFormatter = DateFormatter()
-        let localeStr = "us"
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
-        dateFormatter.locale = Locale(identifier: localeStr)
-        let timePostedFormatted = dateFormatter.date(from: timePosted)
-        let now = Date()
-        var interval = now.timeIntervalSince(timePostedFormatted!)
-        var intervalStr = ""
-        interval = interval / 3600
-        if (interval < 1){
-            interval = interval * 60
-            let intervalInt = Int(interval)
-            intervalStr = String(intervalInt) + "m"
-        }else{
-            var intervalInt = Int(interval)
-            if (intervalInt > 23){
-                intervalInt = (intervalInt / 24)
-                if (intervalInt > 364){
-                    intervalStr = String(intervalInt / 365) + "y"
 
+        if (!self.arrayEmpty){
+            cell.notifImage.layer.borderWidth = 0
+            cell.notifImage.layer.borderColor = UIColor.clear.cgColor
+            cell.userNameLabel.text = self.notificationArray[indexPath.row].userName
+            if self.notificationArray[indexPath.row].type == "checkIn"{
+                cell.notificationLabel.text = "checked in"
+            }else if self.notificationArray[indexPath.row].type == "comment"{
+                cell.notificationLabel.text = "commented on your post."
+            }else if self.notificationArray[indexPath.row].type == "meetUp"{
+                cell.notificationLabel.text = "and you met up!"
+            }
+            let timePosted = notificationArray[(indexPath as NSIndexPath).row].actionTime
+            cell.notifImage.image = self.uiImageArr[indexPath.row]
+            cell.notifImage.contentMode = UIViewContentMode.scaleAspectFit
+            DispatchQueue.main.async(execute: {
+            if (self.notificationArray[(indexPath as NSIndexPath).row].type != "meetUp"){
+                cell.notifImage.layer.borderWidth = 2
+                cell.notifImage.layer.borderColor = UIColor.white.cgColor
+                }})
+
+            let dateFormatter = DateFormatter()
+            let localeStr = "us"
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+            dateFormatter.locale = Locale(identifier: localeStr)
+            let timePostedFormatted = dateFormatter.date(from: timePosted)
+            let now = Date()
+            var interval = now.timeIntervalSince(timePostedFormatted!)
+            var intervalStr = ""
+            interval = interval / 3600
+            if (interval < 1){
+                interval = interval * 60
+                let intervalInt = Int(interval)
+                intervalStr = String(intervalInt) + "m"
+            }else{
+                var intervalInt = Int(interval)
+                if (intervalInt > 23){
+                    intervalInt = (intervalInt / 24)
+                    if (intervalInt > 364){
+                        intervalStr = String(intervalInt / 365) + "y"
+
+                    }else{
+                        intervalStr = String(intervalInt) + "d"
+
+                    }
                 }else{
-                    intervalStr = String(intervalInt) + "d"
+                    intervalStr = String(intervalInt) + "h"
 
                 }
-            }else{
-                intervalStr = String(intervalInt) + "h"
-
             }
+            
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.removeFromSuperview()
+            cell.timeLabel.text = intervalStr
+        
+        }else if (self.arrayEmpty){
+             DispatchQueue.main.async(execute: {
+            for v  in cell.subviews{
+            v.removeFromSuperview()
+            }
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.removeFromSuperview()
+            
+            self.emptyArrayLabel = UILabel(frame: CGRect(x: 0, y: ((self.tableView?.frame.height)! / 2) - 75, width: self.view.frame.width, height: 50))
+            self.tryAgainButton = UILabel(frame: CGRect(x: 0, y: ((self.tableView?.frame.height)! / 2) - 50, width: self.view.frame.width, height: 50))
+            self.tryAgainButton.text = "Tap to retry"
+            self.tryAgainButton.textAlignment = .center
+            self.tryAgainButton.textColor = UIColor.white
+            self.tryAgainButton.layer.masksToBounds = true
+            
+            self.emptyArrayLabel.text = "You have no notifications"
+            self.tryAgainButton.font = UIFont.boldSystemFont(ofSize: 16)
+            self.emptyArrayLabel.textColor = UIColor.white
+            self.emptyArrayLabel.textAlignment = .center
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.removeFromSuperview()
+            self.refreshControl.endRefreshing()
+            
+            cell.addSubview(self.tryAgainButton)
+            cell.addSubview(self.emptyArrayLabel)
+            })
+        
+        
         }
-        
-        self.activityIndicator.stopAnimating()
-        self.activityIndicator.removeFromSuperview()
-        cell.timeLabel.text = intervalStr
-        
+    
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        if (self.arrayEmpty){
+            self.emptyArrayLabel.removeFromSuperview()
+            self.tryAgainButton.removeFromSuperview()
+            progressBarDisplayer("Loading", true)
+            AWSService().loadUser(self.userID,completion: {(result)->Void in
+                
+                self.user = result
+                self.getNotificationArray()
+                
+            })
+        }else{
        
-        AWSService().loadImage(self.notificationArray[indexPath.row].imageID,completion: {(result)->Void in
-             self.chosenImage = self.uiImageArr[indexPath.row]
-            self.chosenImageObj = result
-             self.performSegue(withIdentifier: "viewPostFromNotifications", sender: 1)
-        })
+            AWSService().loadImage(self.notificationArray[indexPath.row].imageID,completion: {(result)->Void in
+                 self.chosenImage = self.uiImageArr[indexPath.row]
+                self.chosenImageObj = result
+                 self.performSegue(withIdentifier: "viewPostFromNotifications", sender: 1)
+            })
+        }
        
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath)-> CGFloat{
+        if (self.arrayEmpty){
+            return self.view.frame.height
+        }else{
+            return UITableViewAutomaticDimension
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
